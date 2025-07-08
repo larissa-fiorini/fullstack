07f4d37d4d9cebe25c1e26b7da3711becb3b7fcb
larissa-fiorini <larissafiorini00@gmail.com>
2025-07-08 16:20:16 -0300

    Setup for prod

--- api/app.js
+++ api/app.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const userRouters = require('./routes/userRoutes')
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+const allowedOrigins = [
+  'http://localhost:5000',
+  'https://fullstack-rqgj.onrender.com',
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 //Routes
 app.use('/users', userRouters);
